67554e6f6fa914b71cb1ea5fec8ecad21723afce
Adolfo Builes <builes.adolfo@gmail.com>
2013-09-11 11:31:05 -0300

    Setup rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dailyember::Application.config.secret_key_base = 'f20fca09fa6aa1301b1b22dd68056548112969c87b52ac820465bce9c2abbf743e21cb0edbebdd4c6c080e1baeb76b06ea583f21d1f3329e9a7d2133b1795b19'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dailyember::Application.config.session_store :cookie_store, key: '_dailyember_session'
